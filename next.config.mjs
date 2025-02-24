24772ba66004f1a0cdeb82d1b4b7214ed4459c1f
maman <maman@automatalabs.co>
2025-02-24 22:41:25 +0700

    update
    Signed-off-by: maman <maman@automatalabs.co>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output:"export",
+    distDir:"out",
     basePath:"",
-    trailingSlash: true
+    assetPrefix:""
 };
 
 export default nextConfig;
